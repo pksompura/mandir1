19883dadedc6b4c60ee60ce81d0e3bbe865c8a96
diva-sl <divakaran.sivasamy@gmail.com>
2025-06-04 15:34:31 +0530

    hsjdjs

--- src/utils/imageUrl.js
+++ src/utils/imageUrl.js
@@ -1,7 +1,6 @@
 // utils/apiBase.js
-const isLocal = window.location.hostname === 'localhost';
-
+const isLocal = window.location.hostname === "localhost";
 
 export const IMAGE_BASE_URL = isLocal
-  ? 'http://localhost:5001'
-  : 'http://88.222.214.214/api';
+  ? "http://localhost:5001"
+  : "https://88.222.214.214/api";
