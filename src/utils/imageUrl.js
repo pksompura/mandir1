a04741088ff42dc2c2aaf27e529af1da018104e6
diva-sl <divakaran.sivasamy@gmail.com>
2025-06-06 14:41:40 +0530

    jajkasjs

--- src/utils/imageUrl.js
+++ src/utils/imageUrl.js
@@ -3,4 +3,4 @@ const isLocal = window.location.hostname === "localhost";
 
 export const IMAGE_BASE_URL = isLocal
   ? "http://localhost:5001"
-  : "https://88.222.214.214/api";
+  : "https://giveaze.com";
